5301c511dfa35afc2e141ec0e50eea49fb725ab4
Safin-Ali <safin.ali.7205@gmail.com>
2023-11-15 07:32:15 +0600

    fix: :bug: tailwind config not auto suggetion problem fixed
    when trying to create own utilities within extend property the tsconfig file not suggetion me css js property name

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,16 @@ module.exports = {
 	content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
 	theme: {
 		extend: {
+			gridTemplateColumns:{
+				'14': 'repeat(14, minmax(0, 1fr))',
+			},
 			colors:{
 				'nav-black':'#1A171E',
 				'primary-bg-black':'#0D0D0F',
 				'primary-red':'#eb1c24',
-				'primary-text':'#d6d6d6',
+				'primary-text':'#dae6f1',
 				'primary-grey':'#b0afb6'
-			}
+			},
 		},
 	},
 	plugins: [],
