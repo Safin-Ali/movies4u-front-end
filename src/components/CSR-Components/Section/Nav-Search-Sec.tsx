274c30733880408be59b4ef603218a7af0e8dbf0
Safin-Ali <safin.ali.7205@gmail.com>
2023-11-15 10:44:20 +0600

    feat: :sparkles: new Nav-Search-Sec UI component without functionality

---
+++ src/components/CSR-Components/Section/Nav-Search-Sec.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { LuSearch } from 'react-icons/lu';
+import { BsGridFill } from 'react-icons/bs';
+import { genres } from '@/data/nav-items';
+import GenreBtn from '../Button/Genre-Btn';
+import useArrExpand from '@/hooks/useArrExpand';
+import SeeAllBtn from '../Button/See-All-Btn';
+
+export default function NavSearchSec() {
+
+	const { handleArr, slicedArr,arrExpanded } = useArrExpand({
+		arr: genres,
+		initLeng: 6
+	});
+
+	return (
+		<div>
+
+			{/* search feild */ }
+			<div
+				className={ `d-v-center search-feild-container` }
+			>
+				<div className={ `text-primary-text` }>
+					<LuSearch />
+				</div>
+
+				<div>
+					<input
+						type='text'
+						className={ `bg-transparent focus:outline-none` }
+					/>
+				</div>
+
+				<div>
+					<BsGridFill />
+				</div>
+			</div>
+
+			<div
+				className={ `my-7` }
+			>
+				<div className={`d-v-center justify-between`}>
+					<h6 className={ `text-sm` }>GENRE</h6>
+					<SeeAllBtn
+						resFun={() => handleArr()}
+					>
+						{arrExpanded ? 'Collapse' : 'See all'}
+					</SeeAllBtn>
+				</div>
+			</div>
+
+			<div
+				className={ `g-full-center grid-cols-2 gap-2` }
+			>
+				{
+					slicedArr.map((gn, gnKey) => {
+						return <GenreBtn key={ gnKey }>{ gn }</GenreBtn>;
+					})
+				}
+			</div>
+		</div>
+	);
+}
