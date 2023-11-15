9c32481e7e2d1f35a4231b4591ad6dd4e4d619c6
Safin-Ali <safin.ali.7205@gmail.com>
2023-11-15 11:28:31 +0600

    refactor: :art: new template Component for root or home page
    that will be persist layout for all path and can be changed

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,15 +1,34 @@
+import LeftSideNav from '@/components/CSR-Components/Appbar/Left-Side-Nav';
+import RightSideNav from '@/components/CSR-Components/Appbar/Right-Side-Nav';
 import React from 'react';
 
-export default function template ({
+export default async function Template({
 	children,
 }: {
 	children: React.ReactNode
 }) {
 
 	return (
-		<h1>
-			i am template
-			{children}
-		</h1>
+		<section
+			className={ `grid grid-cols-14` }
+		>
+			<div
+				className={ `col-span-2` }
+			>
+				<LeftSideNav />
+			</div>
+
+			<main
+				className={ `col-span-9 text-center` }
+			>
+				{children}
+			</main>
+
+			<div
+				className={ `col-span-3` }
+			>
+				<RightSideNav />
+			</div>
+		</section>
 	);
 }
